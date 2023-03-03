ca878cf6edb470c0cad142e920c52c8bc7d28e1d
Mohit <mohitsharma8864@gmail.com>
2023-03-03 14:49:37 +0530

    get and post request

--- tutorial4.py
+++ tutorial4.py
@@ -9,9 +9,11 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        return render_template("login.html")
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
     else:
-        
+        return render_template("login.html")
+
 @app.route("/<usr>")
 def user(usr):
     return f"<h1>{usr}</h1>"
